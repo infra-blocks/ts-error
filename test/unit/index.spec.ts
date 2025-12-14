b42e1e1a992ab92f1317d276f6e26974282ca859
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-12-14 11:05:01 -0500

    bump: @infra-blocks/test@0.6.0
    - Leverage exported expectTypeOf to do type assertions in tests

--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -1,6 +1,5 @@
-import { expect } from "@infra-blocks/test";
+import { expect, expectTypeOf } from "@infra-blocks/test";
 import { isNumber, isObjectNotNull } from "@infra-blocks/types";
-import { assert, type IsExact } from "conditional-type-checks";
 import { findCause, findCauseByType } from "../../src/index.js";
 
 describe("index", () => {
@@ -21,7 +20,7 @@ describe("index", () => {
       it("should return the argument when there are no causes and it matches", () => {
         const root = new Error("root");
         const result = findCause(root, (e) => e === root);
-        assert<IsExact<unknown | undefined, typeof result>>(true);
+        expectTypeOf(result).toEqualTypeOf<unknown | undefined>();
         expect(result).to.equal(root);
       });
       it("should return the first matching cause in the chain", () => {
@@ -29,7 +28,7 @@ describe("index", () => {
         const mid = new Error("trunk", { cause: leaf });
         const root = new Error("root", { cause: mid });
         const result = findCause(root, (e) => e === leaf);
-        assert<IsExact<unknown | undefined, typeof result>>(true);
+        expectTypeOf(result).toEqualTypeOf<unknown | undefined>();
         expect(result).to.equal(leaf);
       });
     });
@@ -37,7 +36,7 @@ describe("index", () => {
       it("should infer the right type with a typeof operator", () => {
         const err = "root";
         const result = findCause(err, (e) => typeof e === "string");
-        assert<IsExact<string | undefined, typeof result>>(true);
+        expectTypeOf(result).toEqualTypeOf<string | undefined>();
         expect(result).to.equal(err);
       });
       it("should infer the right type with an instanceof operator", () => {
@@ -47,7 +46,7 @@ describe("index", () => {
           err,
           (e): e is CustomError => e instanceof CustomError,
         );
-        assert<IsExact<CustomError | undefined, typeof result>>(true);
+        expectTypeOf(result).toEqualTypeOf<CustomError | undefined>();
         expect(result).to.equal(err);
       });
       it("should infer the right type with explicit type guard", () => {
@@ -59,7 +58,7 @@ describe("index", () => {
         };
         const root = new Error("root", { cause: { code: 42 } });
         const result = findCause(root, isCustomError);
-        assert<IsExact<CustomError | undefined, typeof result>>(true);
+        expectTypeOf(result).toEqualTypeOf<CustomError | undefined>();
         expect(result).to.deep.equal({ code: 42 });
       });
     });
@@ -70,7 +69,7 @@ describe("index", () => {
       const mid = new TypeError("trunk", { cause: leaf });
       const root = new RangeError("root", { cause: mid });
       const result = findCauseByType(root, SyntaxError);
-      assert<IsExact<SyntaxError | undefined, typeof result>>(true);
+      expectTypeOf(result).toEqualTypeOf<SyntaxError | undefined>();
       expect(result).to.be.undefined;
     });
     it("should work with a regular constructor", () => {
@@ -78,7 +77,7 @@ describe("index", () => {
       const mid = new TypeError("trunk", { cause: leaf });
       const root = new RangeError("root", { cause: mid });
       const result = findCauseByType(root, SyntaxError);
-      assert<IsExact<SyntaxError | undefined, typeof result>>(true);
+      expectTypeOf(result).toEqualTypeOf<SyntaxError | undefined>();
       expect(result).to.equal(leaf);
     });
     it("should work with an abstract constructor", () => {
@@ -89,7 +88,7 @@ describe("index", () => {
       const mid = new TypeError("trunk", { cause: leaf });
       const root = new RangeError("root", { cause: mid });
       const result = findCauseByType(root, CustomError);
-      assert<IsExact<CustomError | undefined, typeof result>>(true);
+      expectTypeOf(result).toEqualTypeOf<CustomError | undefined>();
       expect(result).to.equal(leaf);
     });
   });
